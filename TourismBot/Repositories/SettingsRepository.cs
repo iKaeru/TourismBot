85deb5507deea58a38cfd6bffa61ced249a6e8bd
iKaeru <antitery@mail.ru>
2020-06-02 00:43:26 +0300

    Repository finished

--- TourismBot/Repositories/SettingsRepository.cs
+++ TourismBot/Repositories/SettingsRepository.cs
@@ -1,10 +1,15 @@
+using System.IO;
+
 namespace TourismBot.Repositories
 {
     public static class SettingsRepository
     {
+        private static readonly string FileAddressPath = string.Format("..{0}..{0}..{0}..{0}Setting.txt", Path.DirectorySeparatorChar);
+
         public static string GetSetting()
         {
-            
+            string[] lines = File.ReadAllLines(FileAddressPath);
+            return lines[0];
         }
     }
 }
